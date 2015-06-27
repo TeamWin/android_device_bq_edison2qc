2f19905aa1f830a28c795c76c9a93444328e1ed7
Kra1o5 <kra1o5x@gmail.com>
2015-06-27 21:29:08 +0200

    Updates
    Change-Id: I6fe421e6f7a0df818baf2892afa3d8e5ce5f1e4c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,19 +28,19 @@ BOARD_CUSTOM_BOOTIMG_MK := device/bq/edison2qc/mkbootimg.mk
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun/file
 
 # Recovery
-TARGET_RECOVERY_FSTAB := device/bq/edison2qc/recovery.fstab
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_HAS_LARGE_FILESYSTEM := true
 BOARD_HAS_NO_SELECT_BUTTON := true
 
 # TWRP specific build flags
-DEVICE_RESOLUTION := 1280x800
+TW_THEME := landscape_hdpi
+TW_DISABLE_DOUBLE_BUFFERING_CHECK := true
 RECOVERY_SDCARD_ON_DATA := true
-TW_BOARD_CUSTOM_GRAPHICS := ../../../device/bq/edison2qc/recovery/graphics.c
-TW_NO_SCREEN_TIMEOUT := true
-TW_INCLUDE_JB_CRYPTO := true
 TW_INTERNAL_STORAGE_PATH := "/data/media"
 TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
 TW_EXTERNAL_STORAGE_PATH := "/external_sd"
 TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+TW_NO_SCREEN_TIMEOUT := true
+TW_NO_CPU_TEMP := true
+TW_INCLUDE_CRYPTO := true
 TW_BRIGHTNESS_PATH := /sys/devices/platform/rk29_backlight/backlight/rk28_bl/brightness
